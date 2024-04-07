07ddbecb40f8d18d61a27a931ad3eb0c86761cf0
L-SA <64151599+L-A-SAS@users.noreply.github.com>
2024-04-07 13:33:44 +0300

    Update about.php

--- admin/about.php
+++ admin/about.php
@@ -7,10 +7,7 @@ defined('admin') or exit;
 
 <div class="content-block">
 
-    <p>The system is a minimal, fast, secure, and dependency-free innovative shopping cart developed by <a href="https://codeshack.io" target="_blank">CodeShack</a>.</p>
-
-    <p>You are currently using <b>Version 1.1.2</b>. For updates and support, click <a href="https://codeshack.io/package/php/advanced-shopping-cart-system/" target="_blank">here</a>.</p>
-
+    
 </div>
 
 <?=template_admin_footer()?>
